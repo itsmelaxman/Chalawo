a31fe06c4dd220b998ed8b451082eb14c6a8773d
Laxman Magarati <Laxmanmagrati@gmail.com>
2022-11-16 17:48:46 +0545

    setup

---
+++ lib/config/global_config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+const String appName = 'Chalawo'; //App Name
+const String appVersion = '1.0.0'; //App Current version
+const Color appColor = Color(0XFF2876BD);
+const bool showDebugBanner = false;
